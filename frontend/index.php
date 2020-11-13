b3333886d75627acaa6a11fa7b670be698c09893
MatRob0300 <matharobert1@gmail.com>
2020-11-13 13:08:25 +0100

    fixes

--- frontend/index.php
+++ frontend/index.php
@@ -1,12 +1,8 @@
 <?php
 
 require_once '../entities/db.php';
-require_once '../entities/fortbildung.php';
-require_once '../entities/kurs.php';
-require_once '../entities/nimmtteil.php';
 require_once '../entities/teilnehmer.php';
-require_once '../entities/randomgenerator.php';
-require_once '../controller.php';
+require_once 'controller.php';
 
 
 $aktion = isset($_GET['aktion'])?$_GET['aktion']:'hauptseite';
