d5804e1c7ee407930cdbef4bff271ce1500f9f71
MatRob0300 <matharobert1@gmail.com>
2020-12-11 14:37:33 +0100

    weatherdaten überarbeitet: bekomme jetzt die daten von openWeathermap.org

--- frontend/views/wetterbericht.tpl.php
+++ frontend/views/wetterbericht.tpl.php
@@ -35,10 +35,11 @@
       <!--
       http://daten.buergernetz.bz.it/de/dataset/southtyrolean-weatherservice-weathersouthtyrol
       http://daten.buergernetz.bz.it/services/weather/district/1/bulletin?format=json&lang=de
+      https://openweathermap.org/
       -->
-      <?php
-        $daten = json_decode(file_get_contents("http://daten.buergernetz.bz.it/services/weather/bulletin?format=json&lang=de"),true);
-        //var_dump($daten);
+      <?php/*
+        $daten = json_decode(file_get_contents("https://api.openweathermap.org/data/2.5/weather?lat46.521202=&lon=11.2272281&APPID=1d3c3cf9a55237a528ec15dafe5065a3"),true);
+        var_dump($daten);*/
       ?>
 
       <h1>Wetter</h1>
@@ -49,26 +50,26 @@
             <p>Heute</p>
           </div>
           <div class="temp">
-            <img id="temp-icon" src="<?php echo $daten['today']['stationData'][4]['symbol']['imageUrl']?>" alt="sun">
-            <p><span id="temp-value"><?php echo $daten['today']['stationData'][4]['min']?> bis <?php echo $daten['today']['stationData'][4]['max']?></span><span id="temp-unit">°C</span></p>
+            <img id="temp-icon" src="" alt="sun">
+            <p><span id="temp-value"></span><span id="temp-unit">°C</span></p>
           </div>
           <div class="climate">
-            <p><?php echo $daten['today']['stationData'][4]['symbol']['description']?></p>
+            <p id="weather-description"></p>
           </div>
 
         </div>
-        <div class="tomorrow">
+        <!--<div class="tomorrow">
           <div class="location">
             <p>Morgen</p>
           </div>
           <div class="temp">
-            <img id="temp-icon" src="<?php echo $daten['tomorrow']['stationData'][4]['symbol']['imageUrl']?>" alt="sun">
-            <p><span id="temp-value"><?php echo $daten['tomorrow']['stationData'][4]['min']?> bis <?php echo $daten['tomorrow']['stationData'][4]['max']?></span><span id="temp-unit">°C</span></p>
+            <img id="temp-icon" src="" alt="sun">
+            <p><span id="temp-value"></span><span id="temp-unit">°C</span></p>
           </div>
           <div class="climate">
-            <p><?php echo $daten['tomorrow']['stationData'][4]['symbol']['description']?></p>
+            <p id="weather-description"></p>
           </div>
-      </div>
+      </div>-->
 
     </main>
     <aside>
@@ -105,6 +106,26 @@
         </div>
       </div>
     </aside>
+    <script type="text/javascript">
+  /*  let url = 'https://api.openweathermap.org/data/2.5/weather?q=Bozen,it&units=metric&APPID=1d3c3cf9a55237a528ec15dafe5065a3&lang=de';
 
+      fetch(url)
+      .then(res => res.json())
+      .then((out) => {
+        var maxtemp = out['main']['temp_max'];
+        var description = out['weather'][0]['description'];
+        var wi1 = "http://openweathermap.org/img/wn/";
+        var wi2 = "@2x.png";
+        var weathericon = out['weather'][0]['icon'];
+        var weatheralt = out['weather'][0]['main'];
+        document.getElementById("weather-description").innerHTML = description;
+        document.getElementById("temp-icon").src = wi1.concat(weathericon, wi2);
+        document.getElementById("temp-icon").alt = weatheralt;
+        document.getElementById("temp-value").innerHTML = maxtemp;
+        console.log('Checkout this JSON! ', out);
+      })
+      .catch(err => { throw err });*/
+
+    </script>
   </body>
 </html>
