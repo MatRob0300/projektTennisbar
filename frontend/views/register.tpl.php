e074bcfbf4927c756e9a76453ee030856db60f96
MatRob0300 <matharobert1@gmail.com>
2021-01-07 11:12:25 +0100

    änderungen

--- frontend/views/register.tpl.php
+++ frontend/views/register.tpl.php
@@ -2,9 +2,55 @@
 <html lang="de">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles/register-styles.css">
     <title></title>
   </head>
   <body>
+    <div class="container">
+      <div class="reg">
+        <div class="header">
+          <h1>Registrierung</h1>
+        </div>
+      </div>
+      <div class="reg">
+        <form class="" action="" method="post">
+          <label for="name">Vorname*:</label>
+          <input type="text" name="name" placeholder="Vorname"><br>
 
+          <label for="surname">Nachname*:</label>
+          <input type="text" name="surname" placeholder="Nachname"><br>
+
+          <label for="mail">E-Mail*:</label>
+          <input type="text" name="mail" placeholder="E-Mail-Adresse"><br>
+
+          <label for="password">Passwort*:</label>
+          <input type="password" name="password" placeholder="Passwort"><br>
+
+          <label for="password-w">Passwort wiederholen*:</label>
+          <input type="password" name="password-w" placeholder="Passwort wiederholen"><br>
+
+          <label for="tel">Telefon*:</label>
+          <input type="text" name="tel" placeholder=""><br>
+
+          <label for="gb">Geburtsdatum:</label>
+          <input type="date" name="gb" placeholder=""><br>
+
+          <label for="plz">PLZ:</label>
+          <input type="text" name="plz" placeholder="">
+
+          <label for="ort">Ort:</label>
+          <input type="text" name="ort" placeholder="Ort"><br>
+
+          <label for="strasse">Straßenname:</label>
+          <input type="text" name="strasse" placeholder="Straßenname">
+
+          <label for="strassenr">Nr.</label>
+          <input type="text" name="strassenr" placeholder="Straßennummer"><br>
+
+          <input type="submit" name="reg" value="registrieren"><br>
+        </form>
+      </div>
+    </div>
+    <footer></footer>
   </body>
 </html>
--- frontend/views/reservierungErstellen.tpl.php
+++ frontend/views/reservierungErstellen.tpl.php
@@ -4,16 +4,20 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" type="text/css" href="styles/reservierung-erstellen-styles.css">
-
   </head>
   <body>
     <header>
       <h1>Platzreservierung</h1>
     </header>
     <main>
-      <label for="date">Wählen Sie das Datum aus:</label>
-      <input type="date" name="date">
-      <input type="submit" name="" value="">
+      <div class="container">
+        <div class="sel-l">
+          https://medium.com/@nitinpatel_20236/challenge-of-building-a-calendar-with-pure-javascript-a86f1303267d
+        </div>
+        <div class="sel-r">
+
+        </div>
+      </div>
     </main>
   <script src="models/re.js" type="text/javascript"></script>
   </body>
