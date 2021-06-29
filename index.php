8719957622cc94fd97947d46ebf9d6d5dbe8a952
MatRob0300 <matharobert1@gmail.com>
2021-06-29 15:15:07 +0200

    frontend Ordner entfernt

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-require_once '../entities/db.php';
-require_once '../entities/benutzer.php';
-require_once '../entities/kommentar.php';
-require_once '../entities/reservierung.php';
+require_once 'entities/db.php';
+require_once 'entities/benutzer.php';
+require_once 'entities/kommentar.php';
+require_once 'entities/reservierung.php';
 require_once 'models/function.inc.php';
 require_once 'controller.php';
 
